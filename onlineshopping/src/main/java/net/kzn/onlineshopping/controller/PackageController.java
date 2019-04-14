f24d68969817b78f925ea4680e3042a2d46340e0
rakesh <rakeshbc.bc@gmail.com>
2019-04-14 13:11:05 +0530

    Completed the interface theme and other issues.

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PackageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PackageController.java
@@ -11,27 +11,26 @@ public class PackageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome Buji");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 
-	@RequestMapping(value = { "/test" })
-	public ModelAndView param(@RequestParam(value = "greeting", required = false) String greeting) {
-		if (greeting == null) {
-			greeting = "Default";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-	}
-	/*
-	@RequestMapping(value ="/test/{greeting}")
-	public ModelAndView param(@PathVariable("greeting") String greeting) {
-		if (greeting == null) {
-			greeting = "Default";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
-		return mv;
-	}*/
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;				
+	}	
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {		
+		ModelAndView mv = new ModelAndView("page");		
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;				
+	}	
+
 }
